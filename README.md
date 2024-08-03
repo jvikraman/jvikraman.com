ed350901a0b4231a184528531089366c2c6e96f8
jvikraman <jvikraman@outlook.com>
2024-08-02 22:48:16 -0400

    (feat): initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jegadeesh's personal blog
+
+Source code for my personal blog powered by Next.js and Tailwind CSS. Deployments via Vercel over a custom domain.
+
+Blog adapted from [Tailwind-Nextjs-Starter-Blog](https://github.com/timlrx/tailwind-nextjs-starter-blog) with additional customizations.
---
+++ components/Header.tsx
@@ -0,0 +1,48 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo-bold.svg'
+import Link from './Link'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+import SearchButton from './SearchButton'
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between py-10">
+      <div>
+        <Link href="/" aria-label={siteMetadata.headerTitle}>
+          <div className="flex items-center justify-between">
+            <div className="mr-3">
+              <Logo className="h-10 w-10 sm:h-16 sm:w-16 md:h-20 md:w-20 fill-blue-400 dark:fill-sky-400 hover:opacity-70 dark:hover:opacity-90" />
+            </div>
+            {typeof siteMetadata.headerTitle === 'string' ? (
+              <div className="hover:gradient-light dark:hover:gradient-dark hidden h-10 text-3xl font-bold sm:block">
+                {siteMetadata.headerTitle}
+              </div>
+            ) : (
+              siteMetadata.headerTitle
+            )}
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+        {headerNavLinks
+          .filter((link) => link.href !== '/')
+          .map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="dark:hover:gradient-dark hover:gradient-light hidden font-medium text-gray-700 dark:text-gray-100 sm:block"
+            >
+              {link.title}
+            </Link>
+          ))}
+        <SearchButton />
+        <ThemeSwitch />
+        <MobileNav />
+      </div>
+    </header>
+  )
+}
+
+export default Header
