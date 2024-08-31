d247f28090e908107f1bb9205cddae9eabfc4cda
Jegadeesh Vikramanthampi <jegadeeshvv@gmail.com>
2024-08-31 11:11:17 -0400

    (feat): add new aws ai cert (#4)
    Co-authored-by: jvikraman <jvikraman@outlook.com>

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -2,8 +2,10 @@ import { ReactNode } from 'react'
 import type { Authors } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
-import ociGenAICertImage from '/public/static/images/oci-genai-cert.png'
-import ociAIFoundCertImage from '/public/static/images/oci-ai-found-cert.png'
+import ociGenAICertImage from '/public/static/images/oci-genai-clear.png'
+import ociAIFoundCertImage from '/public/static/images/oci-ai-found-clear.png'
+import awsAICert from '/public/static/images/aws-ai-cert-clear.png'
+import awsAIAdopter from '/public/static/images/aws-ai-early-adopter-clear.png'
 import CustomImage from 'next/image'
 
 interface Props {
@@ -55,17 +57,17 @@ export default function AuthorLayout({ children, content }: Props) {
           <div className="gradient-light dark:gradient-dark text-center text-2xl font-semibold">
             Certifications
           </div>
-          <div className="flex flex-col items-center justify-center gap-8 py-4 lg:flex-row">
+          <div className="flex flex-col items-center justify-center gap-8 py-4 md:flex-row md:flex-wrap">
             <div className="flex items-center">
               <div className="overflow-hidden">
                 <CustomImage
                   src={ociGenAICertImage}
                   alt="oci certified professional"
                   quality={100}
-                  className="w-[180px] object-cover md:w-[200px] lg:w-[220px]"
+                  className="cursor:pointer w-[120px] object-cover"
                 />
               </div>
-              <div>
+              <div className="hidden">
                 <a
                   className="gradient-tr-light-clip dark:gradient-dark text-center text-lg font-semibold transition hover:opacity-80 hover:dark:opacity-80 md:text-xl"
                   target="_blank"
@@ -82,10 +84,10 @@ export default function AuthorLayout({ children, content }: Props) {
                   src={ociAIFoundCertImage}
                   alt="oci certified professional"
                   quality={100}
-                  className="w-[180px] object-cover md:w-[200px] lg:w-[220px]"
+                  className="w-[120px] object-cover "
                 />
               </div>
-              <div>
+              <div className="hidden">
                 <a
                   className="gradient-tr-light-clip dark:gradient-dark text-center text-lg font-semibold transition hover:opacity-80 hover:dark:opacity-80 md:text-xl"
                   target="_blank"
@@ -93,6 +95,46 @@ export default function AuthorLayout({ children, content }: Props) {
                   href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=4BA6211937C90400E1DAD34E2EA2E8A78329DC4A60C78D9B8F9917DA62689F17"
                 >
                   OCI AI Foundations Associate - 1Z0-1122-24
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="overflow-hidden">
+                <CustomImage
+                  src={awsAICert}
+                  alt="aws certified ai practitioner"
+                  quality={100}
+                  className="w-[120px] object-cover "
+                />
+              </div>
+              <div className="hidden">
+                <a
+                  className="gradient-tr-light-clip dark:gradient-dark text-center text-lg font-semibold transition hover:opacity-80 hover:dark:opacity-80 md:text-xl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.credly.com/badges/17d2cfae-7b65-4729-8814-0bc9f5b04b66/public_url"
+                >
+                  AWS Certified AI Practitioner
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="overflow-hidden">
+                <CustomImage
+                  src={awsAIAdopter}
+                  alt="aws certified ai practitioner"
+                  quality={100}
+                  className="w-[120px] object-cover "
+                />
+              </div>
+              <div className="hidden">
+                <a
+                  className="gradient-tr-light-clip dark:gradient-dark text-center text-lg font-semibold transition hover:opacity-80 hover:dark:opacity-80 md:text-xl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.credly.com/badges/18c8edc1-198c-422b-8d5b-104ecee87a43/public_url"
+                >
+                  AWS Early AI Adopter
                 </a>
               </div>
             </div>
